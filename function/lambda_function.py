758a463a905d59923f36f889bdf3183b4b329e07
rcorales <47377687+rcorales@users.noreply.github.com>
2022-10-12 15:26:28 +0800

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n---------------------------------------------')
+    print('Startin functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
 
